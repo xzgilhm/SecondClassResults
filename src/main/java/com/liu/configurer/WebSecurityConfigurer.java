a85021d610a8b6ea768457e33c88c7fceff72ac9
DESKTOP-2997GI4\73559 <735599488@qq.com>
2017-12-28 20:13:13 +0800

    配置拦截器

---
+++ src/main/java/com/liu/configurer/WebSecurityConfigurer.java
@@ -0,0 +1,63 @@
+package com.liu.configurer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by 73559 on 2017/12/28.
+ */
+@Configuration
+public class WebSecurityConfigurer extends WebMvcConfigurerAdapter {
+
+    /**
+     * 登录session key
+     */
+    public final static String SESSION_KEY = "username";
+
+    @Bean
+    public SecurityInterceptor getSecurityInterceptor() {
+        return new SecurityInterceptor();
+    }
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration addInterceptor = registry.addInterceptor(getSecurityInterceptor());
+
+        // 排除配置
+        addInterceptor.excludePathPatterns("/error");
+        addInterceptor.excludePathPatterns("/login");
+
+
+        // 拦截配置
+        addInterceptor.addPathPatterns("/**");
+    }
+
+
+    private class SecurityInterceptor extends HandlerInterceptorAdapter {
+
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+                throws Exception {
+            //登陆不做拦截
+            if(request.getRequestURI().equals("/login")){
+                return true;
+            }
+            HttpSession session = request.getSession();
+            if (session.getAttribute(SESSION_KEY) != null){
+                return true;
+            }
+            // 跳转登录
+            String url = "login";
+            response.sendRedirect(url);
+            return false;
+        }
+
+    }
+}
--- src/main/java/com/liu/web/UserController.java
+++ src/main/java/com/liu/web/UserController.java
@@ -1,12 +1,18 @@
 package com.liu.web;
 
+import com.liu.configurer.WebSecurityConfigurer;
+import com.liu.core.Result;
+import com.liu.core.ResultGenerator;
 import com.liu.model.TUser;
 import com.liu.service.TRoleService;
 import com.liu.service.TUserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
@@ -21,20 +27,44 @@ public class UserController {
     @Resource
     private TRoleService tRoleService;
 
-    @GetMapping(value = "/hello")
+    @GetMapping(value = "/login")
     public String say(){
         return "login";
     }
 
-    //登陆
+    //转向学生
+    @GetMapping(value = "/student")
+    public String index(@SessionAttribute(WebSecurityConfigurer.SESSION_KEY) String account,Model model){
+        model.addAttribute("username",account);
+        return "student/application";
+    }
+
+    //登陆,并记录session
     @PostMapping(value = "/login")
     @ResponseBody
-    public String login(TUser userlogin)  {
+    public Result login(TUser userlogin, HttpSession session)  {
         System.out.println("---------login.do ------------------");
-//        TUser user = tUserService.findByName(userlogin.getName());
 
-//       System.out.println(user.getPassword());
+        try{
+            TUser tuser = tUserService.findBy("name",userlogin.getName());
+            if(tuser.getPassword().equals(userlogin.getPassword())){
+                // 设置session
+                session.setAttribute(WebSecurityConfigurer.SESSION_KEY, tuser.getName());
+                System.out.println("---session--"+session.getAttribute(WebSecurityConfigurer.SESSION_KEY)+"\n");
+                return ResultGenerator.genSuccessResult(tuser);
+            }
+            else {
+                return ResultGenerator.genFailResult("用户名或者密码不正确");
+            }
+        } catch (NullPointerException e){
+            return ResultGenerator.genFailResult("用户名不存在");
+        }
+    }
 
-        return "-1";
+    //移除session
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute(WebSecurityConfigurer.SESSION_KEY);
+        return "redirect:/login";
     }
 }
